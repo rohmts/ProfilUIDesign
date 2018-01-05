796a0ab9fce53e00d1424b120803774e5858a87e
pesanmerpati <rohmatsuseno@gmail.com>
2018-01-05 21:02:35 +0700

    version 1.0

--- app/src/main/java/com/pesanmerpati/profiluidesign/MainActivity.java
+++ app/src/main/java/com/pesanmerpati/profiluidesign/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pesanmerpati.profiluidesign;
 
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CollapsingToolbarLayout mCollapsingToolbarLayout = findViewById(R.id.collapsingToolBarLayout_toolbar);
+        mCollapsingToolbarLayout.setTitle("Rohmat Demejo");
     }
 }
